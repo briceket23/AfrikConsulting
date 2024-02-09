d3d0fe67357374119bd6430ba54e9be4557fd3e0
briceket23 <147540864+briceket23@users.noreply.github.com>
2024-02-09 10:15:11 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ import Pricing from "@/app/landing-sections/pricing/pricing";
 import Swift from "@/app/landing-sections/swift/page";
 import Partner from "@/app/landing-sections/pertners/partner";
 import Services from "./landing-sections/services/page";
-import { CarouselPlugin } from "./landing-sections/caroussel/page";
+import CarouselAFC from "./landing-sections/caroussel/page";
 import Hero from "./landing-sections/hero/page";
 
 
 export default function Home() {
   return (
     <main className='max-w-6xl mx-auto px-4'>
-      <CarouselPlugin/>
+      <CarouselAFC/>
       <Hero/>
 
       <Swift />
